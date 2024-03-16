e48cb14c1552d2c3aa9d7c4fb580b9243e473064
ohusse <78609674+ohusse@users.noreply.github.com>
2024-03-16 12:51:31 -0600

    main

---
+++ personal_portfolio-project/certifications/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Certification(models.Model):
+    title = models.CharField(max_length = 100)
+    date = models.DateField(editable=True)
+    image = models.ImageField(upload_to='portfolio/images/certification')
+    url = models.URLField(blank=True)
+    skills = models.TextField()
+
+    def __str__(self):
+        return self.title
---
+++ personal_portfolio-project/certifications/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Certification
+
+def all_certifications(request):
+    certifications = Certification.objects.order_by('date')
+    return render(request, 'certifications/all_certifications.html', {'certifications':certifications})
+
